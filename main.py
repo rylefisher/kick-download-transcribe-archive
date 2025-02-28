720a461fb82df21129480fd5ac2a6ee916d4ac09
Sam A <98753696+samfisherirl@users.noreply.github.com>
2025-02-28 10:19:03 -0500

    notation
    xxxx

--- main.py
+++ main.py
@@ -14,18 +14,17 @@ def main():
     transcriber_ = transcriber.Transcriber(log_manager=log_manager_)
     vods = firefox_linkgrabber.main(FIREFOX)
 
-    # Get already downloaded VODs
+    # bypass for testing purposes
     downloaded_vods = log_manager_.get_downloaded_vods()
-    # downloaded_vod_ids = {vod["id"] for vod in downloaded_vods}
     clean_vods = []
     for vod in vods:
         if "jstlk/videos" in vod:
             clean_vods.append(vod.replace("/jstlk/videos/jstlk/videos/", "/jstlk/videos/"))
     vods = clean_vods
-    # Filter VODs
+
+    # Filter VODs - bypass for testing purposes
     # vods_to_download = log_manager_.filter_vods(vods, downloaded_vod_ids)
-    vods_to_download = []
-    vods_to_download = downloaded_vods
+
     for vod in vods:
         # Log downloaded VOD
         idn = vod.split('/')[-1]
