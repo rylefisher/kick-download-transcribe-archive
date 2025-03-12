26f7df4fb3fb273257f39d317cefa7c8d3c520e9
Sam A <98753696+rylekittenhouse@users.noreply.github.com>
2025-03-12 01:40:40 -0500

    fix missing headers

--- required/ytdlp.py
+++ required/ytdlp.py
@@ -28,6 +28,8 @@ def download_video(url: str, base_download_path: str = ".") -> str:
         "720p60/best",  # Format priority
         "-o",
         outtmpl,  # Output template
+        "--add-header",
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         url,
     ]
     try:
